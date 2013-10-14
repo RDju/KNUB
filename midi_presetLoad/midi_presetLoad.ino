515838f7cc381280a5b8b329a6f6d8288a5d090c
eddycombo <eddy@combosquare.net>
2013-10-14 10:26:36 +0200

    added knubUtils.h

--- midi_presetLoad/midi_presetLoad.ino
+++ midi_presetLoad/midi_presetLoad.ino
@@ -3,14 +3,11 @@
 #include "Wire.h"
 #include "memory.h"
 #include "presets.h"
+#include "knubUtils.h"
 #include "knubMidi.h"
 
- 
 
-
-
-
-unsigned long time;
+ unsigned long time;
 
 byte presetIndx  = 5;
 
@@ -22,7 +19,7 @@ void setup(){
 	midiSerial.begin(31250);
 
 	Wire.begin();
-
+	/*
 	Serial.print("writing at address : ");
 	
 	Serial.println(presetIndx*presetSize);
@@ -47,105 +44,18 @@ void setup(){
 	delay(1000);
 	presetIndx = 5;
 	Serial.println("ready");
+	*/
+
 
 
 }
-/*	
-for(int  i =0; i<3;i++){
 
-
-	Serial.print("writing at address : ");
-	Serial.println(presetIndx*presetSize);
-	if(i == 0){
-		writeKnubPreset(eepromAddr1, presetIndx*presetSize, &preset1);
-	}else if(i == 1){
-
-		writeKnubPreset(eepromAddr1, presetIndx*presetSize, &preset2);
-
-	}else{
-
-		writeKnubPreset(eepromAddr1, presetIndx*presetSize, &preset3);
-
-	}
-	delay(500);
-	readKnubPreset(eepromAddr1, presetIndx*presetSize, &currentPreset);
-	delay(1000);
-	printCurrentPreset();
-	delay(2000);
-	presetIndx ++;
-	}	
-
-}	
-*/
 
  void loop(){
 
-	
+ 	midiInRead();
+
 
 }
 
-
-void printCurrentPreset(){
-
-
-	Serial.println(currentPreset.name);
- 	
- 	Serial.println("----------------------");
-   	
-   	Serial.println("KNUBBIES: ");
-
-   	for(int i = 0; i<numKnubbies; i++){
-   			Serial.println(currentPreset.knubbies[i].name);
-
-   		}   	
- 	
- 	Serial.println("----------------------");
-   	
-   	Serial.println("KNUBBIE 1 PARAMS : ");
-
-   	for(int i = 0; i<paramLength; i++){
-
-   		Serial.println(currentPreset.knubbies[0].params[i]);
-	}
- 
-
-	Serial.println("KNUBBIE 1 modOn: ");
-	Serial.println(currentPreset.knubbies[0].modOn);
-	Serial.println("KNUBBIE 1 STATE: ");
-	Serial.println(currentPreset.knubbies[0].state);
-	Serial.println("KNUBBIE 1 NUMLOOP: ");
-	Serial.println(currentPreset.knubbies[0].numLoop);
-
-	Serial.println("----------------------");
-   	Serial.println("KNUBBIE 2 PARAMS : ");
-
-   	for(int i = 0; i<paramLength; i++){
-
-   		Serial.println(currentPreset.knubbies[1].params[i]);
-	}
- 
-
-	Serial.println("KNUBBIE 2 modOn: ");
-	Serial.println(currentPreset.knubbies[1].modOn);
-	Serial.println("KNUBBIE 2 STATE: ");
-	Serial.println(currentPreset.knubbies[1].state);
-	Serial.println("KNUBBIE 2 NUMLOOP: ");
-	Serial.println(currentPreset.knubbies[1].numLoop);
-
-	Serial.println("----------------------");
-   	Serial.println("KNUBBIE 3 PARAMS : ");
-
-   	for(int i = 0; i<paramLength; i++){
-
-   		Serial.println(currentPreset.knubbies[2].params[i]);
-	}
- 
-
-	Serial.println("KNUBBIE 3 modOn: ");
-	Serial.println(currentPreset.knubbies[2].modOn);
-	Serial.println("KNUBBIE 3 STATE: ");
-	Serial.println(currentPreset.knubbies[2].state);
-	Serial.println("KNUBBIE 3 NUMLOOP: ");
-	Serial.println(currentPreset.knubbies[2].numLoop);
-}
 
